8e806b20a67c49a995f37912ec4f694781a85842
Meghan Spencer <emjayess@gmail.com>
2017-08-23 01:35:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var katzDeli = [];
+
 function takeANumber(katzDeliLine,name) {
   var position = katzDeliLine.indexOf(name) + 1;
   return position;
 }
 
 const deliLine = ['Janet','Cato','Velvet','Jenn'];
-
 takeANumber(deliLine,'Velvet');
+
+function nowServing()
