3fdc9b9deda688e0e7bf502b8b7c00ef3c61a59f
Meghan Spencer <emjayess@gmail.com>
2017-08-23 02:30:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function currentLine(line) {
 
   for (let i = 0; i < line.length; i++) {
     if (line.length === 0) {
-      return "The line is currently empty.";
+      currentLine = "The line is currently empty."
+      return currentLine;
     } else {
       currentLine.push(` ${i + 1}. ${line[i]}`);
     }
