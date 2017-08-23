236b5c58536a6be7404bd067bedb9fc03ce5d984
Meghan Spencer <emjayess@gmail.com>
2017-08-23 02:04:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function takeANumber(katzDeliLine,name) {
   // return `Welcome, ${name}. You are number ${position} in line.`;
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing() {
+  if (katzDeli.length !== 0) {
+    console.log(katzDeli[0]);
+    return katzDeli.shift();
+  } else {
+    console.log("The line is currently empty");
+    return "There is nobody waiting to be served!";
+  }
+}
