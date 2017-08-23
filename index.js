485d63f283f317afc93262ea3c669e9f4625b0ad
Meghan Spencer <emjayess@gmail.com>
2017-08-23 01:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var katzDeli = ['jane'];
 
 function takeANumber(katzDeliLine,name) {
   katzDeli.push(name);
-  console.log(position);
-  console.log(katzDeli);
+  console.log(`Welcome, ${name}. You are number ${katzDeli.length}`;);
   // return `Welcome, ${name}. You are number ${position} in line.`;
   return `Welcome, ${name}. You are number ${katzDeli.length}`;
 }
