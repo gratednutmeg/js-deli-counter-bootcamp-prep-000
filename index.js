6c2d0bffda178390150b1685443c2efdf68dc9e3
Meghan Spencer <emjayess@gmail.com>
2017-08-23 01:53:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var deli = [];
+var katzDeli = [];
 
-function takeANumber(katzDeliLine,name) {
-  deli.push(name);
-  var position = deli.length;
+function takeANumber(katzDeli,name) {
+  katzDeli.push(name);
+  var position = katzDeli.length;
   console.log(position);
-  console.log(deli);
+  console.log(katzDeli);
   return `Welcome, ${name}. You are number ${position} in line.`;
 }
 
 function nowServing() {
-  if (deli.length !== 0) {
-    console.log(deli[0]);
-    return deli.shift();
+  if (katzDeli.length !== 0) {
+    console.log(katzDeli[0]);
+    return katzDeli.shift();
   } else {
     console.log("The line is currently empty");
     return "There is nobody waiting to be served!";
   }
 }
+
