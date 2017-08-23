bb8a4cec3a15addfc6d9523bc2101a7e59a01624
Meghan Spencer <emjayess@gmail.com>
2017-08-23 01:41:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ takeANumber(deliLine,'Velvet');
 function nowServing() {
   if (katzDeli.length !== 0) {
     console.log(katzDeli[0]);
-    return array.shift();
+    return katzDeli.shift();
   } else {
     console.log("The line is currently empty");
     return "There is nobody waiting to be served!";
